4a630354f9f3ec50dad0e9e1d2f6a5c6fcc3f460
Shiming Ge (Centific Technologies Inc) <v-shige@microsoft.com>
2025-05-16 15:41:42 +0800

    experiment for tf-idf and DBSCAN

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-## clustering
+# clustering
 clustering error message for analyzing earlier
 
-# Guide:
+## Guide:
 1. create visual environment: 
     ``` python 
     python -m venv .env
@@ -12,11 +12,19 @@ clustering error message for analyzing earlier
     ```python
     pip install -r requirements.txt
     ```
-# Usage:
+## Usage:
 ``` python
 python -m clustering -sr <result json root path> -pp <path pattern>
 ```
-demo: 
+### demo: 
 ```python
 python -m clustering -sr D:\mytest -pp *\*\*.json
 ```
+
+# clustering using tf-idf and DBSCAN
+## usage:
+```python
+python .\log_clustering.py -rl <raw data path> -esp <float, default value 0.5> -ms <min-samples, defalut value 2>
+
+```
+ 
--- log_clustering.py
+++ log_clustering.py
@@ -1,10 +1,44 @@
 # -*- coding: utf-8 -*-
 
-import re, json, os, logging
+import re, json, os, logging,argparse
 from datetime import datetime
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import DBSCAN
 from itertools import groupby
+
+args = argparse.ArgumentParser()
+args.add_argument(
+    "--epsilon",
+    "-esp",
+    help="The maximum distance between two samples for them to be considered as in the same neighborhood.",
+    type=float,
+    default=0.5,
+)
+
+args.add_argument(
+    "--min_samples",
+    "-ms",
+    help="The minimum number of samples in a neighborhood for a point to be considered a core point.",
+    type=int,
+    default=2,
+)
+
+args.add_argument(
+    "--verbose",
+    "-v",
+    help="The verbosity level.",
+    action="store_true",
+)
+
+args.add_argument(
+    "--raw_logs",
+    "-rl",
+    help="The path of the raw logs.",
+    type=str,
+    default="*.json",
+)
+
+parsed_args = args.parse_args()
 
 def preprocess_log(log):
     """
@@ -19,25 +53,27 @@ def cluster_logs(logs):
     Cluster logs using TF-IDF and DBSCAN.
     """
     # Preprocess logs
+    logging.info("Preprocessing logs...")
     preprocessed_logs = [preprocess_log(log) for log in logs]
+    logging.info(f"Preprocessed {len(preprocessed_logs)} logs.")
     
     # Feature extraction using TF-IDF
+    logging.info("Extracting features using TF-IDF...") 
     vectorizer = TfidfVectorizer()
     X = vectorizer.fit_transform(preprocessed_logs)
+    logging.info(f"Extracted features from {X.shape[0]} logs with {X.shape[1]} features.")
     
     # Clustering using DBSCAN
-    dbscan = DBSCAN(eps=0.5, min_samples=2)
+    logging.info("Clustering logs using DBSCAN...")
+    # DBSCAN parameters
+    dbscan = DBSCAN(eps=parsed_args.epsilon, min_samples=parsed_args.min_samples)
     clusters = dbscan.fit_predict(X)
+    logging.info(f"Clustering completed. ")
     
     return clusters
 
 def configure_logging():
-    # with open('logging_config.yaml', 'r') as f:
     os.makedirs("logs", exist_ok=True)
-    # config = yaml.safe_load(f)
-    # config["handlers"]["file"]["filename"] = f'logs/output_{datetime.now().strftime("%Y-%m-%d_%H_%M_%S_%f")}.log'
-    # # logging.config.dictConfig(config)
-    # logging.StreamHandler()
     logging.FileHandler(f'logs/output_{datetime.now().strftime("%Y-%m-%d_%H_%M_%S_%f")}.log')
     logging.basicConfig(
         level=logging.DEBUG,
@@ -47,18 +83,19 @@ def configure_logging():
             logging.StreamHandler()
         ]
     )
-    # logger = logging.getLogger("clustering")
 
 def main():
     configure_logging()
     logs = list()
-        
     raw_logs = rf"D:\repos\clustering\rawdata\raw_data_1745476397.176719.json"
     raw_logs = rf"D:\repos\clustering\rawdata\raw_data_1747364466.534833.json"
-    logging.info(f'Loading raw logs from {raw_logs}')
-    with open(raw_logs, 'r') as fd:
+    
+    logging.info(f'Loading raw logs from {parsed_args.raw_logs}')
+    logging.info(f'with parameter - epsilon : {parsed_args.epsilon}')
+    logging.info(f'     parameter - min_samples : {parsed_args.min_samples}')
+    with open(parsed_args.raw_logs, 'r') as fd:
         test_result = json.load(fd)
-        print(test_result)
+        logging.info(f'Loaded raw logs from {raw_logs}')
         try:
             if type(test_result) == list:
                 for e in test_result:
@@ -66,23 +103,21 @@ def main():
         except Exception as e:
             logging.exception(f"exception threw when loading raw file: {e}")
     
-    logging.info(f'{len(logs)} logs loaded')
-
+    logging.info(f'Loaded {len(logs)} logs loaded')
     
     # Cluster logs
     clusters = cluster_logs(logs)
+    logging.info(f"Clusters: {len(set(clusters))}")
     
     # Print results
-    zipped_logs = zip(clusters, logs)
-    groupby_logs = groupby(zipped_logs, key=lambda x: x[0])
-    # logs, clusters = zip(*sorted_logs)
-    for cluster, log in groupby_logs:
+    for cluster, log in groupby(sorted(zip(clusters, logs), key=lambda x: x[0]), key=lambda x: x[0]):
         c_logs = list(log)
-        logging.info(f"============= Cluster {cluster}:")
-        for log in c_logs:
-            logging.info(f"Log:{log[1]} ")
-        print("==================================================>\n")
-
+        logging.info(f"============= Cluster {cluster}: [{len(c_logs)}] logs =============")
+        if parsed_args.verbose:
+            for log in c_logs:
+                logging.info(log[1])
+            logging.info("="*75)
+            
 if __name__ == "__main__":
     main()
 
